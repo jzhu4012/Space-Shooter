fec1945ea37f3ff10387f622e8b839c3c98565a1
Jessica Zhu <jzhu4012@gmail.com>
2026-05-27 19:21:15 -0400

    first commit

---
+++ code/main.py
@@ -0,0 +1,58 @@
+# pygame documentation https://pyga.me/docs/
+import pygame
+from os.path import join
+
+from random import randint
+
+# pygame general setup
+pygame.init()
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('Space Shooter')
+running = True
+
+# plain surface
+surf = pygame.Surface((100, 200))
+surf.fill('orange')
+x = 100
+
+# imports
+# Note: make sure to call convert() or convert_alpha() to improve performance
+laser_surf = pygame.image.load(join('images', 'laser.png')).convert_alpha()
+laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT - 20))
+
+meteor_surf = pygame.image.load(join('images', 'meteor.png')).convert_alpha()
+meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
+
+player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
+player_rect = player_surf.get_frect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
+player_direction = 1
+
+star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
+star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
+
+while running:
+    # poll for events
+    for event in pygame.event.get():
+        # pygame.QUIT event means user clicked X to close window
+        if event.type == pygame.QUIT:
+            running = False
+
+    # draw game
+    # blit - block image trasfer
+    display_surface.fill('darkgray')
+    for position in star_positions:
+        display_surface.blit(star_surf, position)
+
+    display_surface.blit(meteor_surf, meteor_rect)
+    display_surface.blit(laser_surf, laser_rect)
+    
+    # player movement
+    player_rect.x += player_direction * 0.4
+    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
+        player_direction *= -1
+    display_surface.blit(player_surf, player_rect)
+
+    pygame.display.update()
+
+pygame.quit()
