5bfe864aa006f3ffb51ce0978ece536593510ae8
Jessica Zhu <jzhu4012@gmail.com>
2026-05-28 13:45:42 -0400

    Implemented vectors for positions and delta time to allow independence between FPS and movement

--- code/main.py
+++ code/main.py
@@ -10,6 +10,7 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Space Shooter')
 running = True
+clock = pygame.time.Clock()
 
 # plain surface
 surf = pygame.Surface((100, 200))
@@ -26,12 +27,14 @@ meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
 
 player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
 player_rect = player_surf.get_frect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
-player_direction = 1
+player_direction = pygame.math.Vector2(1, 1)
+player_speed = 300
 
 star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
 star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
 
 while running:
+    dt = clock.tick() / 1000
     # poll for events
     for event in pygame.event.get():
         # pygame.QUIT event means user clicked X to close window
@@ -48,9 +51,11 @@ while running:
     display_surface.blit(laser_surf, laser_rect)
     
     # player movement
-    player_rect.x += player_direction * 0.4
+    if player_rect.bottom > WINDOW_HEIGHT or player_rect.top < 0:
+        player_direction.y *= -1
     if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
-        player_direction *= -1
+        player_direction.x *= -1
+    player_rect.center += player_direction * player_speed * dt
     display_surface.blit(player_surf, player_rect)
 
     pygame.display.update()
